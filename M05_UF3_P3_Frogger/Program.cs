d482f92dd7908af2fe1f9c2e6eb244cba21e7413
MAYA07\maryk <marykulungrai63@gmail.com>
2023-05-15 16:21:01 +0200

    cambio en main

--- M05_UF3_P3_Frogger/Program.cs
+++ M05_UF3_P3_Frogger/Program.cs
@@ -8,7 +8,52 @@ namespace M05_UF3_P3_Frogger
     {
         static void Main(string[] args)
         {
-            //TODO
+            Console.CursorVisible = false;
+            Console.SetWindowSize(Utils.MAP_WIDTH, Utils.MAP_HEIGHT);
+
+            Player player = new Player();
+            List<Lane> lanes = GenerateLanes();
+
+
+            Utils.GAME_STATE gameState = Utils.GAME_STATE.RUNNING;
+
+            while (gameState == Utils.GAME_STATE.RUNNING)
+            {
+                Vector2Int input = Utils.Input();
+                gameState = player.Update(input, lanes);
+
+                Console.Clear();
+
+                foreach (Lane lane in lanes)
+                {
+                    lane.Update();
+                    lane.Draw();
+                }
+
+                player.Draw(lanes);
+
+                TimeManager.NextFrame();
+            }
+
+            Console.Clear();
+
+            if (gameState == Utils.GAME_STATE.WIN)
+            {
+                Console.WriteLine("Felicidades!Has Ganado");
+            }
+            else if (gameState == Utils.GAME_STATE.LOOSE)
+            {
+                Console.WriteLine("¡Has Perdido!");
+            }
+
+            Console.ReadKey();
+            static List<Lane> GenerateLanes()
+            {
+                List<Lane> lanes = new List<Lane>();
+
+           
+                return lanes;
+            }
         }
     }
 }
